5b066385494ef76c62f801668b213f5c69141f29
DGuardado19 <37202984+DGuardado19@users.noreply.github.com>
2018-06-08 14:49:01 -0600

    Creando la abstract factory que crea las razas

---
+++ src/parcial2poo/Demacia/FactoryDemacia.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial2poo.Demacia;
+
+import parcial2poo.AbstratFactory.AbstractRazas;
+import parcial2poo.AbstratFactory.Demacia;
+import parcial2poo.AbstratFactory.Noxus;
+import parcial2poo.AbstratFactory.Vacio;
+
+/**
+ *
+ * @author DGuardado <david.guardado at guardado.org>
+ */
+public class FactoryDemacia implements AbstractRazas {
+
+    @Override
+    public Demacia getDemacia(String s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Noxus getNoxus(String s) {
+        return null; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Vacio getVacio(String s) {
+        return null; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
